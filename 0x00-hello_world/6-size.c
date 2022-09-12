e13eb64ca72644a6266a38a98eeb01cd0aa2df56
SandraDammy <89700539+SandraDammy@users.noreply.github.com>
2022-09-12 16:18:27 +0100

    Update 6-sizes.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	
+	
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of a int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return(0);
+}
