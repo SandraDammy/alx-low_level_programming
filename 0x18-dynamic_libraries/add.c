c6be0d359f0b9cf72213182ebff797e01c401bc0
SandraDammy <89700539+SandraDammy@users.noreply.github.com>
2022-12-13 14:53:23 +0100

    update add

--- 0x18-dynamic_libraries/add.c
+++ 0x18-dynamic_libraries/add.c
@@ -1,1 +1,15 @@
-testing
+#include "main.h"
+/**
+* add - adds two given integers
+*
+* @a: first integer
+* @b: second integer
+*
+* Return: sum of the two integers
+*/
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
