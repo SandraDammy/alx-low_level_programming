f8484ad31bd97e114dbb399956974d2ed98e7bcf
SandraDammy <89700539+SandraDammy@users.noreply.github.com>
2022-12-13 14:56:32 +0100

    update mod

--- 0x18-dynamic_libraries/mod.c
+++ 0x18-dynamic_libraries/mod.c
@@ -1,1 +1,15 @@
-testing
+#include "main.h"
+/**
+* mod - adds two given integers
+*
+* @a: first integer
+* @b: second integer
+*
+* Return: sum of the two integers
+*/
+int mod(int a, int b)
+{
+int mod;
+mod = a % b;
+return (mod);
+}
